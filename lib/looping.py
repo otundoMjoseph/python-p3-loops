2d48eda4288abebb88fe800c66129d3149b1e6b1
JosephMainaOtundo <otundomainajoseph@gmail.com>
2024-05-26 08:36:37 +0300

    all tests passing

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for i in range(10, 0, -1):
+        print(i)
+    print("Happy New Year!")
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+    
+
+def square_integers(numbers):
+  squared_numbers = []
+  for number in numbers:
+    squared_numbers.append(number * number) 
+  return squared_numbers
+    
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+  for i in range(1, 101):
+    if i % 15 == 0:
+      print("FizzBuzz")
+    elif i % 3 == 0:
+      print("Fizz")
+    elif i % 5 == 0:
+      print("Buzz")
+    else:
+      print(i)
